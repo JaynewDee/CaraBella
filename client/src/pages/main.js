4d0b802e579c024a983229d1f61f97eb57ed28b5
NuminousBlue <jdiehl2236@gmail.com>
2022-05-03 06:12:25 -0500

    testing deployment

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Banner from '../components/Banner';
 import MainSection from '../components/MainSection';
 import Footer from '../components/Footer';
-import Frame from '../components/Frame'
+import Frame from '../components/Frame';
 
 
 const Main = () => {
