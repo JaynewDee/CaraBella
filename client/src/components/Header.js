8a998a40108d331d8050a422893c447daed58b6a
NuminousBlue <jdiehl2236@gmail.com>
2022-05-03 04:16:05 -0500

    testing mobile deployment

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,12 +5,15 @@ const Header = () => {
   const andperse = '&';
   
   return (
+
     <header id="fixedBar">
+
       <div id="titleWrap">
-         <h1 id="brandName">CARA BELLA </h1>
+         <h1 onClick={((e) => {window.location.replace('/')})}id="brandName">CARA BELLA </h1>
          <span className="divider"> | </span>
          <h2 id="subtitle"> SPA {andperse} AESTHETICS</h2>
       </div>
+
       <nav id="linkBox">
 
             <Link to="/contact" className="link">CONTACT</Link>
@@ -18,10 +21,12 @@ const Header = () => {
             <Link to="/services" className="link">SERVICES</Link>
             <span className="divider"> | </span>
             <Link to="/about"className="link">ABOUT</Link>
+
       </nav>
 
     </header>
+
   )
 }
 
-export default Header
+export default Header;
